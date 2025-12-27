0d643cc2e05034f162cee5dddaf48232ab002b88
rajanayakdevops <rajanayak.devops@gmail.com>
2025-12-27 10:24:44 +0530

    added the frontent file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
-import AppRoutes from "./routes/AppRoutes";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <AppRoutes />;
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch(import.meta.env.VITE_BACKEND_URL + "/api/test")
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Vite React Working ✅</h1>
+      <h2>{message}</h2>
+    </div>
+  );
 }
 
 export default App;
