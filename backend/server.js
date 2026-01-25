3451c12ebba556094336ef2f62e55514a08c343f
rajanayakdevops <rajanayak.devops@gmail.com>
2026-01-25 22:02:55 +0530

    minor change in the code

--- backend/server.js
+++ backend/server.js
@@ -104,5 +104,7 @@ io.on("connection", (socket) => {
   });
 });
 
+
+// this server running on port 5000 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
